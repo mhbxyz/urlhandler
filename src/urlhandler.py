5492036d8b4a6f4b0deb434b274322a585d57acf
Manoah BERNIER <hello@numbergazing.com>
2022-10-10 19:29:32 +0200

    Add default attributes to class URLHandler

--- src/urlhandler.py
+++ src/urlhandler.py
@@ -1,4 +1,16 @@
+from urllib.parse import urlparse
 
 
 class URLHandler:
-    pass
+
+    scheme: str = 'https'
+    user_info: str = None
+    host: str = None
+    port: int = None
+    path: str = None
+    query: dict = None
+    query_delimiter: str = '&'
+    fragments: list = []
+
+    def __init__(self):
+        pass
