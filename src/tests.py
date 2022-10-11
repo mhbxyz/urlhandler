80b1b1f50582ff7b61ef32560afb417222f5e0fa
Manoah BERNIER <hello@numbergazing.com>
2022-10-11 12:49:17 +0200

    Add a test routine for URLHandler.get_url

--- src/tests.py
+++ src/tests.py
@@ -10,3 +10,7 @@ class TestURLHandler(TestCase):
         self.assertEqual(URLHandler._is_a_number('-42'), -42)
         self.assertEqual(URLHandler._is_a_number('-42.0'), -42.0)
         self.assertEqual(URLHandler._is_a_number('None'), None)
+
+    def test_get_url(self):
+        url = 'http://user:password@127.0.0.1?name=value&False=false&True=true&None=nulldict={1:"one","two":2,"three":"three"}#forward'
+        self.assertEqual(URLHandler(url).get_url(), url)
