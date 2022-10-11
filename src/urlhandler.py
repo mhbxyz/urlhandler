2bd8a5578971a54843b42b52cf2a154bfa6de156
Manoah BERNIER <hello@numbergazing.com>
2022-10-11 07:52:16 +0200

    Add checks to get some metadata about the url composition

--- src/urlhandler.py
+++ src/urlhandler.py
@@ -1,16 +1,19 @@
-import re
 
 
 class URLHandler:
 
     scheme: str = 'https'
     user_info: dict = None
+    has_user_info: bool = False
     host: str = None
     port: int = None
     path: str = None
+    end_with_slash: bool = False
     query: dict = {}
+    has_query: bool = False
     query_delimiter: str = '&'
     fragment: str = None
+    has_fragment: bool = False
 
     _SCHEME_REGEX = r'^(?P<scheme>[a-z]+)://'
     _USER_INFO_REGEX = r'://(?P<user>\w+):(?P<password>[\w\W]+)@'
@@ -21,4 +24,14 @@ class URLHandler:
     _FRAGMENT_REGEX = r''
 
     def __init__(self, url: str = None):
-        pass
+
+        url = url.strip()
+
+        if '@' in url:
+            self.has_user_info = True
+        if url[-1] == '/':
+            self.end_with_slash = True
+        if '?' in url:
+            self.has_query = True
+        if '#' in url:
+            self.has_fragment = True
