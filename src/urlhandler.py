e421b0c35946f356e9a2793bacd1951c19554814
Manoah BERNIER <hello@numbergazing.com>
2022-10-11 07:07:28 +0200

    Add local variables to hold regexes - Add regex to get url scheme - Add regex to get user info

--- src/urlhandler.py
+++ src/urlhandler.py
@@ -1,3 +1,4 @@
+import re
 
 
 class URLHandler:
@@ -9,7 +10,15 @@ class URLHandler:
     path: str = None
     query: dict = {}
     query_delimiter: str = '&'
-    fragments: str = None
+    fragment: str = None
 
-    def __init__(self):
+    _SCHEME_REGEX = r'^(?P<scheme>[a-z]+)://'
+    _USER_INFO_REGEX = r'://(?P<user>\w+):(?P<password>[\w\W]+)@'
+    _HOST_REGEX = r''
+    _PORT_REGEX = r''
+    _PATH_REGEX = r''
+    _QUERY_REGEX = r''
+    _FRAGMENT_REGEX = r''
+
+    def __init__(self, url: str = None):
         pass
