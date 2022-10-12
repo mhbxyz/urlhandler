840e048f2220b0d641141bc0005be0a537eaccad
Manoah BERNIER <hello@numbergazing.com>
2022-10-12 10:17:38 +0200

    Move test url outside of the test into a private global variable

--- src/tests.py
+++ src/tests.py
@@ -2,6 +2,15 @@ from unittest import TestCase
 
 from urlhandler import URLHandler
 
+_DEFAULT_URL = 'http://user:password' \
+              '@127.0.0.1' \
+              '?name=value' \
+              '&False=false' \
+              '&True=true' \
+              '&None=null' \
+              '&dict={"1":"one","two":2,"three":"three"}' \
+              '#forward'
+
 
 class TestURLHandler(TestCase):
     def test__is_a_number(self):
@@ -12,5 +21,4 @@ class TestURLHandler(TestCase):
         self.assertEqual(URLHandler._is_a_number('None'), None)
 
     def test_get_url(self):
-        url = 'http://user:password@127.0.0.1?name=value&False=false&True=true&None=null&dict={"1":"one","two":2,"three":"three"}#forward'
-        self.assertEqual(URLHandler(url).get_url(), url)
+        self.assertEqual(URLHandler(_DEFAULT_URL).get_url(), _DEFAULT_URL)
